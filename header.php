33bc6e0f218613aa2c34e203882c503e223cd0a9
LC <lc.luochao@gmail.com>
2020-05-21 18:48:29 +0800

    Second commi

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
                                                 <li><a href="blog.php">Blog</a></li>
                                                 <li><a href="single-blog.php">Blog Details</a></li>
                                                 <li><a href="elements.php">Elements</a></li>
-                                                <li><a href="job_details.php">job Details</a></li>
+                                                <!-- <li><a href="job_details.php">job Details</a></li> -->
                                             </ul>
                                         </li>
                                         <li><a href="contact.php">Contact</a></li>
